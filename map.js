fd9c7759662e998de4c69b8d7dd97dec57274749
J1008sianG <90944809+J1008sianG@users.noreply.github.com>
2021-10-12 22:41:53 +0800

    Add files via upload

--- map.js
+++ map.js
@@ -3,3 +3,13 @@ vegaEmbed("#choroplethMap", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_2 = "choroplethFertility.json";
+vegaEmbed("#choroplethFertility", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "choroplethMedian.json";
+vegaEmbed("#choroplethMedian", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
--- plot.js
+++ plot.js
@@ -1,4 +1,9 @@
-var vg_2 = "scatterPlot.json";
-vegaEmbed("#scatterPlot", vg_2).then(function(result) {
+var vg_1 = "scatterPlot.json";
+vegaEmbed("#scatterPlot", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "pieChart.json";
+vegaEmbed("#pieChart", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
